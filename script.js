73c637794dd50a7433e0bd5868f1d21bcdda3615
abedalrhmanrizk <abedalrhman.rizk.com>
2023-05-06 12:05:40 +0300

    add edite button

--- script.js
+++ script.js
@@ -13,18 +13,28 @@ function adTodo() {
   if (!todoText) return;
 
   const todoEl = document.createElement("li");
-
+  const editeBtn = document.createElement("i");
+  editeBtn.classList.add("fa-regular");
+  editeBtn.classList.add("fa-pen-to-square");
   todoEl.textContent = todoText;
+  todoEl.appendChild(editeBtn);
 
-  todoEl.addEventListener("click", () =>
-    todoEl.classList.toggle("completed")
-  );
+  // todoEl.addEventListener("click", () =>
+  //   todoEl.classList.toggle("completed")
+  // );
+
+  editeBtn.addEventListener("click", () => {
+    input.value = todoEl.textContent;
+    console.log(todoText);
+    todoEl.remove();
+  });
 
   todoEl.addEventListener("contextmenu", (e) => {
     e.preventDefault();
     todoEl.remove();
   });
 
+  // todosUL.appendChild(todoEl);
   todosUL.insertAdjacentElement("afterbegin", todoEl);
   input.value = "";
 }
